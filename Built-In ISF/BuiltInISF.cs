245cfa411b1bd9579de2668506e09fefb5e5c3ec
Alexis <alexis.morin.mail@gmail.com>
2020-05-15 18:47:52 -0400

    mvp

---
+++ Built-In ISF/BuiltInISF.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Rendering;
+
+[ExecuteAlways]
+public class BuiltInISF : MonoBehaviour
+{
+
+    [SerializeField]
+    Shader deferredShading = null;
+
+    [SerializeField]
+    bool enableInverseSquareFalloff = true;
+
+    void OnValidate()
+    {
+        if (enableInverseSquareFalloff)
+        {
+            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, deferredShading);
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.UseCustom);
+
+            RegenerateFalloff();
+        }
+        else
+        {
+            GraphicsSettings.SetCustomShader(BuiltinShaderType.DeferredShading, Shader.Find("Hidden/Internal-DeferredShading"));
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.Disabled);
+            GraphicsSettings.SetShaderMode(BuiltinShaderType.DeferredShading, BuiltinShaderMode.UseBuiltin);
+        }
+
+
+        AssetDatabase.Refresh();
+    }
+    void OnEnable()
+    {
+        RegenerateFalloff();
+    }
+    void Start()
+    {
+        RegenerateFalloff();
+    }
+    void OnScriptsReloaded()
+    {
+        RegenerateFalloff();
+    }
+
+    void RegenerateFalloff()
+    {
+        GraphicsSettings.lightsUseLinearIntensity = true;
+        GraphicsSettings.lightsUseColorTemperature = true;
+
+
+        // True Attenuation
+        int pixelCount = 16;
+        Texture2D m_AttenTex = new Texture2D(pixelCount, 1, TextureFormat.ARGB32, false, true);
+        m_AttenTex.filterMode = FilterMode.Bilinear;
+        m_AttenTex.wrapMode = TextureWrapMode.Clamp;
+        Color[] pixels = new Color[pixelCount * pixelCount];
+        Vector2 center = new Vector2(0, pixelCount / 2);
+        int blackLimit = pixelCount - 1;
+        int maxDistance = 10;
+
+        for (int i = 1; i <= pixelCount; i++)
+        {
+            float v;
+
+            if (i < blackLimit)
+            {
+                float normalizedIntensity = (float)i / blackLimit;
+                float linearIntensity = normalizedIntensity * maxDistance;
+                v = 1.0f / (linearIntensity * linearIntensity);
+            }
+            else
+                v = 0.0f;
+
+            pixels[i - 1] = new Color(v, v, v, v);
+        }
+
+        m_AttenTex.SetPixels(pixels);
+        m_AttenTex.Apply();
+        Shader.SetGlobalTexture("_LightTexturePoint", m_AttenTex);
+        Shader.SetGlobalTexture("_LightTextureSpot", m_AttenTex);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Built-In-ISF
- Inverse Square Falloff for Built-In Unity Lights 
+## Inverse Square Falloff for Built-In Unity Lights 
+
+Around a year ago I shipped my [Falloff Customizer](https://github.com/alexismorin/Falloff-Customizer). While useful, it's still a little wonky and overengineered for what I want: Inverse Square Falloff in the Built-In Unity Render Pipeline!
+
+![falloff](images/falloff.png)
+
+With the built-in renderer, Unity lights use a linear falloff model. In nearly every single instance, this won't by what you want or compare to lighting in other game engines (or the Unity URP/HDRP)! Especially if you're using linear color space and the deferred render path, you'll probably want this issue fixed as soon as possible. With this small gizmo, you can basically just drop a script onto your Camera and then toggle ISF on or off.
+
+![gif](images/gif.gif)
+
+### How to Use
+
+**This assumes you are using linear lighting and the deferred render path. I don't really use forward rendering**
+
+1. Drag in the "Built-In SIM" folder anywhere in your project.
+2. Place the *BuiltInISF* script somewhere in your scene. You can switch the falloff per-scene with this script!
+3. From the folder, drag the *DeferredShadingOverride* shader onto the shader slot in the *BuiltInISF* scipt! How this script works is that it overrides Unity's stock deferred rendering shader for one with different falloff values.
+4. Toggle the checkbox on an off - you should see falloff for point and spot lights change accordingly!
